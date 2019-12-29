8d0385444db421fde4b8ed452a9d89400fc6a9db
Gustavo Gomes <gustavo.sgomess@gmail.com>
2019-12-29 19:40:41 -0300

    Treinamento javascript 2 Alura

---
+++ app/ts/models/index.ts
@@ -0,0 +1,6 @@
+export * from './Negociacoes';
+export * from './Negociacao';
+export * from './NegociacaoParcial';
+export * from './Imprimivel';
+export * from './Igualavel';
+export * from './MeuObjeto';
